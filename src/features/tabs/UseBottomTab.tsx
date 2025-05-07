6bfddd49210c8daa9d80b7f047c7e0d845cf94a6
NiromiKaumini <138492534+NiromiKaumini@users.noreply.github.com>
2025-05-07 13:27:58 +0530

    create home screen folder

---
+++ src/features/tabs/UseBottomTab.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const UseBottomTab = () => {
+  return (
+    <View>
+      <Text>UseBottomTab</Text>
+    </View>
+  )
+}
+
+export default UseBottomTab
